6edeb60fd0211fccdbaa2edba44b37fc9e93cfd6
Frank Tonya <francktonya7@gmail.com>
2023-03-29 22:18:25 +0200

    Initialisation de l'application cliente et integration au serveur

--- GestionPersonnesServeur/src/main/java/com/gestion/repositories/PersonneRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gestion.repositories;
-
-import com.gestion.models.Personne;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PersonneRepository extends CrudRepository<Personne, Long> {
-}
